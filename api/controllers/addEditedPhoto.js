08c6a560ce146080646571ad5a2a0afce573e686
Will <whp3652@rit.edu>
2016-05-07 01:44:33 -0400

    Add imgur upload of edited photo

--- api/controllers/addEditedPhoto.js
+++ api/controllers/addEditedPhoto.js
@@ -1,12 +1,24 @@
 import { User } from '../models/user';
+import imgur from 'imgur';
+imgur.setClientId('665e91941704f23');
 
 export default function addEditedPhoto(req, res) {
   return User.find({ _id: req.params.id })
   .then(([u]) => {
-    u.editedPhoto = req.body.link;
     u.memory = req.body.memory;
-    u.state = 'FINISHED';
-    return u.save();
+
+    if (req.body.bigScreen) {
+      u.state = 'APPROVED';
+    }
+    else {
+      u.state = 'FINISHED';
+    }
+
+    return imgur.uploadBase64(req.body.imageData.split(',')[1])
+    .then((res) => {
+      u.editedPhoto = res.data.link;
+      return u.save();
+    });
   })
   .then((u) => res.status(200).json(u));
 }
