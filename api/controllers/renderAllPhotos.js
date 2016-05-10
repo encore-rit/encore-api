167572c72c975381fe1a238901f448e49dd03e7e
Will <whp3652@rit.edu>
2016-05-10 08:36:09 -0400

    Restrict it to sharable photos

--- api/controllers/renderAllPhotos.js
+++ api/controllers/renderAllPhotos.js
@@ -2,7 +2,7 @@ import { User } from '../models/user';
 import { chain, prop, concat } from 'ramda';
 
 export default function renderAllPhotos(req, res) {
-  return User.find({ photos: { $ne: null } })
+  return User.find({ photos: { $ne: null }, state: 'APPROVED' })
   .then((users) => {
     const urls = concat(
       chain(prop('editedPhoto'), users),
